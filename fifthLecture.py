54d22082627ede2e8e9793715300befbc77d0f3b
MansoorAhmadQazi <mansoorqazi71@gmail.com>
2026-01-21 12:28:00 +0500

    fifth lecture data types

---
+++ fifthLecture.py
@@ -0,0 +1,17 @@
+x = 5
+x = "Hello World"
+x = 20
+x = 20.5
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool	
+x = b"Hello"	#bytes	
+x = bytearray(5)	#bytearray	
+# x = memoryview(bytes(5))	#memoryview	
+# x = None
+print(type(x))
